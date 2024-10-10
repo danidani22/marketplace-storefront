074969984af02196686354d8e12da116b9c413b0
Dani Sullivan <nz.danisullivan@gmail.com>
2024-10-10 21:55:26 +1300

    Set up project
    Initial set up for the storefront Next project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/modules/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ['"Kumbh Sans"', 'system-ui', 'sans-serif'],
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        cream: '#F8F8E6',
+        milky: '#FBFBF2',
+        paper: '#FFFFFD',
+        kenya: '#2C2A26',
+        cookie: '#423F38',
+        tea: '#514D45',
+        coffee: '#6E695E',
+        frappe: '#847E71',
+      },
+      borderWidth: {
+        px: '1px',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
