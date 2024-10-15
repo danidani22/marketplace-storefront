fe1e138348152b4dd36a58a3bb295e8907f9f521
Dani Sullivan <nz.danisullivan@gmail.com>
2024-10-15 22:23:53 +1300

    Created not found component

---
+++ src/components/NotFound/index.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="container mx-auto min-h-56 flex justify-center items-center">
+      <h1 className="text-2xl font-medium">Product Not Found</h1>
+    </div>
+  )
+}
