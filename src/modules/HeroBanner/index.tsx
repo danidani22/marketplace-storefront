2d6742b8be375bc31d8b2ee749b52c314b53a14d
Dani Sullivan <nz.danisullivan@gmail.com>
2024-10-15 22:24:39 +1300

    Created hero banner component

---
+++ src/modules/HeroBanner/index.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function HeroBanner() {
+  return (
+    <header className="py-12 flex gap-12 flex-col xl:flex-row">
+      <Image
+        src="/img/banner-placeholder.jpg"
+        width={552}
+        height={552}
+        alt="New Collection of Toys!"
+        className="flex-1 w-full md:w-2/3 lg:1/2 rounded-lg"
+      />
+
+      <div className="flex-1 flex flex-col justify-center items-start gap-4">
+        <h1 className="text-2xl font-semibold">Trending Products</h1>
+        <h2 className="text-5xl font-bold">New Arival From Mans Collection</h2>
+        <p className="text-lg font-medium mt-6 mb-16">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut magna,
+          scelerisque vitae augue et, cursus placerat lorem. In nisi lacus,
+          eleifend tincidunt quam et, consequat semper.
+        </p>
+
+        <Link href="/" className="button">
+          Shop Now
+        </Link>
+      </div>
+    </header>
+  )
+}
